9aed490166d25df5a3bab77f668063ed88b72047
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-01-13 16:57:27 +0100

    Hotovo na párek

--- NakupSeznam/Form1.Designer.cs
+++ NakupSeznam/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // AddClick
@@ -65,16 +66,28 @@
             // 
             // panel2
             // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.panel2.Location = new System.Drawing.Point(129, 49);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(659, 389);
+            this.panel2.Size = new System.Drawing.Size(859, 389);
             this.panel2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(724, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1123, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -92,6 +105,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- NakupSeznam/Form1.cs
+++ NakupSeznam/Form1.cs
@@ -12,6 +12,7 @@ namespace NakupSeznam
 {
     public partial class Form1 : Form
     {
+        List<Item> list = new List<Item>();
         int i = 0;
         int pointXText = 30;
         int pointXBtn = 450;
@@ -26,11 +27,15 @@ namespace NakupSeznam
         {
             i++;
             TextBox a = new TextBox();
+            a.Name = "Text" + i;
             CheckBox ch = new CheckBox();
+            ch.Name = "Check" + i;
             Button btn = new Button();
             btn.Text = "Smazat";
-            btn.Name = "Delete" + i;
+            btn.Name = Convert.ToString(i);
+            string s = btn.Name;
             btn.Location = new Point(pointXBtn, pointYText);
+            btn.Click += new EventHandler(DynamicButton_Click);
             a.Location = new Point(pointXText, pointYText);
             ch.Location = new Point(pointXCheck, pointYText);
             panel2.Controls.Add(btn);
@@ -38,7 +43,43 @@ namespace NakupSeznam
             panel2.Controls.Add(a);
             panel2.Show();
             pointYText += 25;
-           
+            Item item = new Item(btn, a, ch);
+            list.Add(item);
+        }
+        private void DynamicButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int x = 0;
+            panel2.Controls.Remove(btn);
+            for (int y = 0; y < list.Count; y++)
+            {
+                if (list[y].Btn.Name == btn.Name)
+                {
+                    panel2.Controls.Remove(list[y].Btn);
+                    panel2.Controls.Remove(list[y].Check);
+                    panel2.Controls.Remove(list[y].Text);
+                    x = y;
+                }
+            }
+            for (int y = x; y < list.Count; y++)
+            {
+                list[y].Btn.Location = new Point(list[y].Btn.Location.X, list[y].Btn.Location.Y - 25);
+                list[y].Text.Location = new Point(list[y].Text.Location.X, list[y].Text.Location.Y - 25);
+                list[y].Check.Location = new Point(list[y].Check.Location.X, list[y].Check.Location.Y - 25);
+            }
+            pointYText -= 25;
+        }
+    }
+    class Item
+    {
+        public TextBox Text { get; set; }
+        public CheckBox Check { get; set; }
+        public Button Btn { get; set; }
+        public Item(Button Btn, TextBox Text, CheckBox Check)
+        {
+            this.Check = Check;
+            this.Btn = Btn;
+            this.Text = Text;
         }
     }
 }
